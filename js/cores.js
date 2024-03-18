10f29ceed98a90d766af95eb64c9f0a4465ce46b
Júlio Akamine <159172550+JuliooAkamine@users.noreply.github.com>
2024-03-18 20:29:59 +0000

    commit

--- js/cores.js
+++ js/cores.js
@@ -1,5 +1,21 @@
 'use strict'
 
-corazul = document.getElementById('azul')
+let verde = document.getElementById('verde')
+let azul = document.getElementById('azul')
+let laranja = document.getElementById('laranja')
 
-corazulfundo = corazul.document.body.style.backgroundColor = 'blue'
+verde.onmouseover = () => document.body.style.backgroundColor = 'green'
+azul.onmouseover = () => document.body.style.backgroundColor = 'blue'
+laranja.onmouseover = () => document.body.style.backgroundColor = 'orange'
+
+verde.onclick = () => document.getElementById('texto').innerHTML = 'verde significa saúde e esperança'
+
+azul.onclick = () => document.getElementById('texto').innerHTML = 'azul significa harmonia'
+
+
+laranja.onclick = () => document.getElementById('texto').innerHTML = 'laranja significa força e energia'
+
+
+
+
+
---
+++ js/lampada.js
@@ -0,0 +1,15 @@
+
+function passar(){
+    document.getElementById('apagada').style.display = 'none'
+    document.getElementById('ligada').style.display = 'block'
+    
+}
+
+function sair(){
+    document.getElementById('apagada').style.display = 'block'
+    document.getElementById('ligada').style.display = 'none'
+}
+
+function quebrar(){
+  
+}
