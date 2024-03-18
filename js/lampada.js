c974a83354973da14ec48677bc21e8ff3ddf22dc
Júlio Akamine <159172550+JuliooAkamine@users.noreply.github.com>
2024-03-18 23:31:29 +0000

    terminado exercicio lampada

--- js/lampada.js
+++ js/lampada.js
@@ -11,5 +11,13 @@ function sair(){
 }
 
 function quebrar(){
-  
+    document.getElementById('apagada').style.width = '0px'
+    document.getElementById('ligada').style.display = 'none'
+    document.getElementById('quebrada').style.display = 'block'
+}
+
+function resetar(){
+    document.getElementById('apagada').style.display = 'block'
+    document.getElementById('apagada').style.width = '300px'
+    document.getElementById('quebrada').style.display = 'none'
 }
