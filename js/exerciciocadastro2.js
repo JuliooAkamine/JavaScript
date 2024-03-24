9dbabb3661ad7b64fe9ad5ec1261a4933531db7d
Júlio Akamine <159172550+JuliooAkamine@users.noreply.github.com>
2024-03-24 15:59:24 +0000

    exercicio cadastro de notas

--- js/exerciciocadastro2.js
+++ js/exerciciocadastro2.js
@@ -1,52 +1,34 @@
-
-
-let formcontato = document.querySelector('#formcontato')
-let formrecebido = document.querySelector('#formrecebido')
-
-
-
 let nomerecebido = document.querySelector('#namerecebido')
 let emailrecebido = document.querySelector('#emailrecebido')
 let telefonerecebido = document.querySelector('#telefonerecebido')
 let assuntorecebido = document.querySelector('#assuntorecebido')
 let mensagemrecebido = document.querySelector('#mensagemrecebida')
 
-var botaoEnviar = document.getElementById("enviar");
+let btnenviar = document.getElementById("enviar");
 
-botaoEnviar.addEventListener('click', function() {
+btnenviar.addEventListener('click', function(event) {
+    event.preventDefault()
+    
    
     let nome = document.querySelector('#name').value;
     let email = document.querySelector('#email').value;
     let assunto = document.querySelector('#assunto').value
     let telefone = document.querySelector('#telefone').value
+    let mensagem = document.querySelector('#mensagem').value
 
     
-    if (nome === '') {
+    if (nome && email && assunto && telefone && mensagem   === ''  ) {
         console.log('Campo não preenchido');
     } else{
         nomerecebido.innerHTML = nome
-    }
-
-    if (email === '') {
-        console.log('Campo não preenchido');
-    } else{
         emailrecebido.innerHTML = email
-    }
-
-    if (assunto === '') {
-        console.log('Campo não preenchido');
-    } else{
         assuntorecebido.innerHTML = assunto
+        telefonerecebido.innerHTML = telefone
+        mensagemrecebido.innerHTML = mensagem
     }
 
-    if (telefone === '') {
-        console.log('Campo não preenchido');
-    } else{
-        telefonerecebido.innerHTML = telefone
-    }
-       
 }
 );
 
-//meu campo de nome for prenchido.innerhtml = 'valornome'
+//lógica usada - se meu campo não for preenchido exiba campo não preenchido se não mande todos os dados para os campos recebidos
 
---
+++ js/exercicionotas.js
@@ -0,0 +1,52 @@
+var btncalculo = document.getElementById('calculo')
+
+btncalculo.addEventListener('click' , function(){
+
+
+var name = document.getElementById('name').value
+var serie = document.getElementById('serie').value
+var materia = document.getElementById('materia').value
+
+var b1 = parseFloat(document.getElementById('b1').value)
+var b2 = parseFloat(document.getElementById('b2').value)
+var b3 = parseFloat(document.getElementById('b3').value)
+var b4 = parseFloat(document.getElementById('b4').value)
+
+var media = parseFloat(document.getElementById('media').value)
+
+var nomerecebido = document.getElementById('nomerecebido')
+var serierecebido = document.getElementById('serierecebido')
+var materiarecebido = document.getElementById('materiarecebido')
+var b1recebido = document.getElementById('b1recebido')
+var b2recebido = document.getElementById('b2recebido')
+var b3recebido = document.getElementById('b3recebido')
+var b4recebido = document.getElementById('b4recebido')
+var mediarecebido = document.getElementById('mediarecebido')
+var situacao = document.getElementById('situacao')
+
+var bimestressomados = b1 + b2 + b3 + b4
+
+
+
+if(b1 , b2 , b3 , b4, media  >10 ){
+    alert('A nota máxima permitida é 10')
+}else{   
+
+nomerecebido.innerHTML = name
+serierecebido.innerHTML = serie
+materiarecebido.innerHTML = materia
+b1recebido.innerHTML = b1
+b2recebido.innerHTML = b2
+b3recebido.innerHTML = b3
+b4recebido.innerHTML = b4
+mediarecebido.innerHTML = (bimestressomados / 4).toFixed(2)
+if(mediarecebido.innerHTML >= media)
+{
+    situacao.innerHTML = 'Aprovado'
+}else{
+    situacao.innerHTML = 'Reprovado'
+}
+}
+
+
+})
